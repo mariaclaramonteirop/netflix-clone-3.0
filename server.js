cc09ee19b543e6e9029ce2ff6bf14d1092dc31f1
Maria Monteiro <mcmpacheco7@gmail.com>
2024-02-10 19:31:56 -0300

    adicionando integração via API

--- server.js
+++ server.js
@@ -1,15 +1,16 @@
- const express = require("express")
- const path = require("path")
+const express = require("express")
+const path = require("path")
 const port = 3333
 
- let initialPath = path.join(__dirname, "public")
- let app = express()
- app.use(express.static(initialPath))
+let initialpath = path.join(__dirname, "public")
 
- app.get("/", (req, res)=>{
-    res.sendFile(path.join(initialPath, "index.html"))
- } )
+let app = express()
+app.use(express.static(initialpath))
 
- app.listen(port,()=>{
-    console.log(`Server start up on port ${port}!`)
- })
+app.get("/", (req,res)=>{
+    res.sendFile(path.join(initialpath, "index.html"))
+})
+
+app.listen(port, ()=>{
+    console.log(`server start up on port ${port}!`)
+})
